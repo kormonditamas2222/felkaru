4b2197fbdc6a9f16806632c5e8b24fc9153b5004
kormonditamas2222 <t.kormond2006@gmail.com>
2026-05-20 09:21:19 +0200

    még xaml

--- gambling/MainWindow.xaml.cs
+++ gambling/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,8 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace gambling
 {
@@ -16,6 +19,7 @@ namespace gambling
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
